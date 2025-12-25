e53856397937710a30449637fdda179af4dd8066
Ada <ada@chronovore.dev>
2025-12-25 12:53:39 +0530

    repo: remove .net

--- Package.swift
+++ Package.swift
@@ -1,5 +1,4 @@
 // swift-tools-version: 6.0
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 // SPDX-FileCopyrightText: 2024 Legiayayana <ada@chronovore.dev>
 // SPDX-License-Identifier: EUPL-1.2
@@ -9,7 +8,6 @@ import PackageDescription
 let package = Package(
 	name: "Myna",
 	products: [
-		// Products define the executables and libraries a package produces, making them visible to other packages.
 		.library(
 			name: "Myna",
 			targets: ["Myna"])
@@ -19,8 +17,6 @@ let package = Package(
 		.package(url: "https://github.com/apple/swift-algorithms", from: "1.2.0"),
 	],
 	targets: [
-		// Targets are the basic building blocks of a package, defining a module or a test suite.
-		// Targets can depend on other targets in this package and products from dependencies.
 		.target(
 			name: "Myna",
 			dependencies: [
